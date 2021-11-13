5c63e2d50ab8a946bd62cf677949517083cd76f7
meelaniekp <meelaniekp@gmail.com>
2021-11-13 15:48:42 -0300

    entrega7fin

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,8 @@
 let costoEnvio = 1.15;
 
 document.addEventListener("DOMContentLoaded", function(e){
+    
+
     getJSONData(CART_INFO2_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             let articulos = resultObj.data.articles;
@@ -26,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                 total+= subtotal
                 htmlContentToAppend += `
             <tr><td><img src=${articles.src} class="img-fluid img-thumbnail imagen"></td><td>${articles.name}</td>
-            <td> <input type="number" onchange="sumar()" min="1" value="${articles.count}"></td><td>UYU</td><td class="precio">${subtotal}</td></tr>
+            <td> <input class="cantidad" type="number" onchange="sumar()" min="0" value="${articles.count}"></td><td>UYU</td><td class="precio">${subtotal}</td></tr>
             
             `
             } else{
@@ -34,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                 total+= subtotal
                 htmlContentToAppend += `
             <tr><td><img src=${articles.src} class="img-fluid img-thumbnail imagen"></td><td>${articles.name}</td>
-            <td> <input type="number" onchange="sumar()" min="1" value="${articles.count}"></td><td>${articles.currency}</td><td class="precio">${articles.unitCost}</td></tr>
+            <td> <input class="cantidad" type="number" onchange="sumar()" min="0" value="${articles.count}"></td><td>${articles.currency}</td><td class="precio">${articles.unitCost}</td></tr>
             
             `
             }
@@ -87,3 +89,44 @@ function envio(){
 
 }
 
+document.getElementById("customRadio1").addEventListener("click",()=>{
+    document.getElementById("modalnombreTitular").disabled = false;
+    document.getElementById("modalnombreNumero").disabled = false;
+    document.getElementById("modalnombreVto").disabled = false;
+    document.getElementById("modalnombreCvv").disabled = false;
+    document.getElementById("numeroCuenta").disabled = true;
+    document.getElementById("banco").disabled = true;
+});
+
+document.getElementById("customRadio2").addEventListener("click",()=>{
+    document.getElementById("modalnombreTitular").disabled = true;
+    document.getElementById("modalnombreNumero").disabled = true
+    document.getElementById("modalnombreVto").disabled = true;
+    document.getElementById("modalnombreCvv").disabled = true;
+    document.getElementById("numeroCuenta").disabled = false;
+    document.getElementById("banco").disabled = false;
+});
+
+function finalizarCompra(){
+    let cantidad = document.getElementsByClassName("cantidad");
+    let cantidadTotal = 0;
+    let calle = document.getElementById("direccionReg").value
+    let numero = document.getElementById("numeroReg").value
+    let pais = document.getElementById("paisReg").value
+
+    for (let i = 0; i < cantidad.length; i++) {
+        cantidadTotal += parseFloat(cantidad[i].value)
+
+    }
+    if (cantidadTotal !=0 && calle !="" && numero !="" && pais !=""){
+        swal("Compra realizada con éxito", "Disfrute de su compra!", "success", {
+            button: "Volver al carrito",
+          });
+    } else {
+        swal("Su compra no puede ser realizada", "Faltan datos de compra", "error", {
+            button: "Volver al carrito",
+          });
+    }
+
+
+}
